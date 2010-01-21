802f3110c4a41d0aee1d03a81384ab28616f7a90
Scott Shillcock <scott@shillcock.net>
2010-01-21 14:39:45 -0800

    cleaned up the code a bit

--- scripts/uploader.rb
+++ scripts/uploader.rb
@@ -15,18 +15,6 @@ def create_date_array
   [t.year, t.month, t.day, t.hour, t.min, t.sec]
 end
 
-# def generate_hash file
-#   hash_func = Digest::SHA1.new
-#   open(file, 'rb') do |io|
-#     counter = 0
-#     while (!io.eof)
-#       readBuf = io.readpartial(1024)
-#       hash_func.update readBuf
-#     end
-#   end
-#   hash_func.hexdigest
-# end
-
 
 class Uploader
   def initialize args={}
@@ -34,45 +22,56 @@ class Uploader
     @db.recreate!
     @id = nil
     @dir = nil
+    @doc = nil
   end
   
   def start(dir, filter='*.ive')
     @dir = dir
-    Dir[File.join(@dir, filter)].each { |file|
-      process_asset File.join(Dir.pwd, file)
-    }
+    Dir[File.join(@dir, filter)].each { |file| process_asset(File.join(Dir.pwd, file)) }
+  end
+  
+  def save_doc
+    @doc['updated'] = create_date_array
+    response = @db.save_doc @doc
+    @doc['_rev'] = response['rev']
+    response['ok']
+  end
+  
+  def put_attachment(name, data, options)
+    response = @db.put_attachment(@doc, name, data, options)
+    @doc = @db.get @id
+    response['ok']
   end
   
   def process_asset file
     puts "=-=-=- Processing #{File.basename file} -=-=-="
     jsonFile = file + '.json'
     thumbDir = jsonFile + '.tdb'
-    data = File.read jsonFile;
-    asset = JSON.parse data;
-    @id = asset['_id']
-    asset['created'] = create_date_array
-    asset['updated'] = create_date_array
-    asset['current'] = []
-    asset['thumbnails'] = {}
-    response = @db.save_doc asset;
-    puts "Document saved: #{@id}"
-    upload_attachment file
-    upload_thumbnails thumbDir
+    raw = File.read jsonFile
+    data = JSON.parse raw
+    @doc = CouchRest::Document.new data
+    @doc.database = @db
+    @id = @doc['_id']
+    @doc['created'] = create_date_array
+    @doc['current'] = []
+    @doc['thumbnails'] = {}
+    if save_doc
+      puts "Document saved: #{@id}"
+      upload_attachment file
+      upload_thumbnails thumbDir
+    end
   end
   
   def upload_attachment file
     data = File.read file;
-    sha1 = Digest::SHA1.hexdigest(data)
-    # sha1 = generate_hash file
+    sha1 = Digest::SHA1.hexdigest data
     name = sha1 + '.ive'
     type = 'model/x-ive'
-    @doc = @db.get @id
-    response = @db.put_attachment(@doc, name, data, {'content_type' => type})
-    doc = @db.get @id
-    puts "Attachment uploaded: #{name} for #{File.basename file}"
-    doc['current'] << {'attachment' => name, 'content_type' => type}
-    doc['updated'] = create_date_array
-    response = @db.save_doc doc
+    if put_attachment(name, data, {'content_type' => type})
+      puts "Attachment uploaded: #{name}"
+      @doc['current'] << {'attachment' => name, 'content_type' => type}
+      save_doc
+    end
   end
   
   def upload_thumbnails dir
@@ -80,18 +79,17 @@ class Uploader
     type = 'image/png'
     images = []
     Dir[File.join(dir, filter)].each { |file|
-      data = File.read file;
+      data = File.read file
       name = File.basename file
-      doc = @db.get @id
-      response = @db.put_attachment(doc, name, data, {'content_type' => type})
-      print '.'
-      images << name
+      if put_attachment(name, data, {'content_type' => type})
+        print '.'
+        images << name
+      end
     }
     puts " #{images.size} Thumbnails uploaded"
-    doc = @db.get @id
-    doc['thumbnails']['images'] = images
-    doc['updated'] = create_date_array
-    @db.save_doc doc
+    @doc['thumbnails']['images'] = images
+    @doc['updated'] = create_date_array
+    save_doc
   end
 end
 
